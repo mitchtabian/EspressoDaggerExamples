5c7ba9e91691ee76e349e7efc770a400f93fd382
mitch <mitch@tabian.ca>
2020-01-23 09:46:10 -0800

    first commit

---
+++ app/src/main/java/com/codingwithmitch/espressodaggerexamples/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.codingwithmitch.espressodaggerexamples.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codingwithmitch.espressodaggerexamples.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
