85eb31559fc62098014cee75834ee8b75edf873d
Mitch Tabian <mitch@tabian.ca>
2020-02-04 15:41:52 -0800

    added first tests with recyclerview. But it's randomly failing... ex: run the test 5 times. It randomly fails some.

--- app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentTest.kt
@@ -1,11 +1,17 @@
 package com.codingwithmitch.espressodaggerexamples.ui
 
+
+import android.view.View
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Observer
-import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.internal.util.Checks
 import androidx.test.platform.app.InstrumentationRegistry
 import com.codingwithmitch.espressodaggerexamples.R
 import com.codingwithmitch.espressodaggerexamples.TestBaseApplication
@@ -13,18 +19,22 @@ import com.codingwithmitch.espressodaggerexamples.di.DaggerTestAppComponent
 import com.codingwithmitch.espressodaggerexamples.di.TestAppComponent
 import com.codingwithmitch.espressodaggerexamples.fragments.MockFragmentFactory
 import com.codingwithmitch.espressodaggerexamples.util.EspressoIdlingResourceRule
+import com.codingwithmitch.espressodaggerexamples.util.JsonUtil
 import com.codingwithmitch.espressodaggerexamples.util.printLogD
 import com.codingwithmitch.espressodaggerexamples.viewmodels.MainViewModelFactory
 import io.mockk.*
+import kotlinx.android.synthetic.main.layout_blog_list_item.view.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-
-
 import org.junit.runner.RunWith
 import javax.inject.Inject
+import javax.inject.Named
+
 
 @InternalCoroutinesApi
 @ExperimentalCoroutinesApi
@@ -35,6 +45,13 @@ class ListFragmentTest{
 
     @Inject
     lateinit var viewModelFactory: MainViewModelFactory
+
+    @Inject
+    lateinit var jsonUtil: JsonUtil
+
+    @Inject
+    @Named("blog_posts_data_filename")
+    lateinit var blog_posts_filename: String
 
     lateinit var appComponent: TestAppComponent
 
@@ -73,18 +90,51 @@ class ListFragmentTest{
 
         onView(withId(R.id.recycler_view)).check(matches(isDisplayed()))
 
-        scenario.onFragment { fragment ->
+//        scenario.onFragment { fragment ->
 
-            fragment.viewModel.viewState.observe(fragment.viewLifecycleOwner, Observer { viewState ->
-                printLogD(CLASS_NAME, "viewstate: ${viewState}")
-            })
+//            fragment.viewModel.viewState.observe(fragment.viewLifecycleOwner, Observer { viewState ->
+//                printLogD(CLASS_NAME, "viewstate: ${viewState.listFragmentView.blogs}")
 
-        }
+//                val blogPosts = jsonUtil.readJSONFromAsset(blog_posts_filename)
+//                val blogPost = BlogPost(24, "Fdngfdg", "Gdfgfd ", "fdggdfg", "dfgdfgreg")
+//                val blogPost = blogPosts!!.get(0)
+//                assertThat(viewState.listFragmentView.blogs, hasItem(blogPost))
+//            })
+
+//        }
+
+
+//        val blogPosts = jsonUtil.readJSONFromAsset(blog_posts_filename)
+//
+//        val blogPost = BlogPost(24, "Fdngfdg", "Gdfgfd ", "fdggdfg", "dfgdfgreg")
+
+//        onView(withId(R.id.recycler_view))
+//            .perform(RecyclerViewActions.scrollToHolder<BlogPostListAdapter.BlogPostViewHolder>(
+//                withBlogPostTitleInViewHolder("Blake Posing for his Website")
+//            ))
+        onView(withId(R.id.recycler_view))
+            .perform(RecyclerViewActions.scrollToPosition<BlogPostListAdapter.BlogPostViewHolder>(8))
+        onView(withText("Blake Posing for his Website")).check(matches(isDisplayed()))
 
     }
 
 
+    fun withBlogPostTitleInViewHolder(blogPostTitle: String): TypeSafeMatcher<BlogPostListAdapter.BlogPostViewHolder> {
+        Checks.checkNotNull(blogPostTitle)
 
+        return object: TypeSafeMatcher<BlogPostListAdapter.BlogPostViewHolder>(){
+
+            override fun describeTo(description: Description?) {
+                description?.appendText("with blog post title: $blogPostTitle")
+            }
+
+            override fun matchesSafely(item: BlogPostListAdapter.BlogPostViewHolder?): Boolean {
+                printLogD(CLASS_NAME, "matchesSafely: ${(blogPostTitle == item?.itemView?.blog_title?.text.toString())}")
+                return (blogPostTitle == item?.itemView?.blog_title?.text.toString())
+            }
+
+        }
+    }
 
 }
 
