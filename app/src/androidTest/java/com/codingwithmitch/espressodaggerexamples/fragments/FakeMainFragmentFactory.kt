988bf20b2b32abf0e6b3bdf11594d10d563cb92f
mitch <mitch@tabian.ca>
2020-02-28 14:08:41 -0800

    Lecture: MainFragmentFactory Fake

---
+++ app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/fragments/FakeMainFragmentFactory.kt
@@ -0,0 +1,59 @@
+package com.codingwithmitch.espressodaggerexamples.fragments
+
+import androidx.fragment.app.FragmentFactory
+import com.codingwithmitch.espressodaggerexamples.ui.*
+import com.codingwithmitch.espressodaggerexamples.util.GlideManager
+import com.codingwithmitch.espressodaggerexamples.viewmodels.FakeMainViewModelFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.InternalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@InternalCoroutinesApi
+class FakeMainFragmentFactory
+constructor(
+    private val viewModelFactory: FakeMainViewModelFactory,
+    private val uiCommunicationListener: UICommunicationListener,
+    private val requestManager: GlideManager
+): FragmentFactory(){
+
+    override fun instantiate(classLoader: ClassLoader, className: String) =
+
+        when(className){
+
+            ListFragment::class.java.name -> {
+                val fragment = ListFragment(viewModelFactory, requestManager)
+                fragment.setUICommunicationListener(uiCommunicationListener)
+                fragment
+            }
+
+            DetailFragment::class.java.name -> {
+                val fragment = DetailFragment(viewModelFactory, requestManager)
+                fragment.setUICommunicationListener(uiCommunicationListener)
+                fragment
+            }
+
+            FinalFragment::class.java.name -> {
+                val fragment = FinalFragment(viewModelFactory, requestManager)
+                fragment.setUICommunicationListener(uiCommunicationListener)
+                fragment
+            }
+
+            else -> {
+                super.instantiate(classLoader, className)
+            }
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
