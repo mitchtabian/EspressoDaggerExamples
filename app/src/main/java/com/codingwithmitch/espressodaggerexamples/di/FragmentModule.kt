d7c32f64da84c7f12b83e9984af6d08ce4c769ca
mitch <mitch@tabian.ca>
2020-02-28 16:27:59 -0800

    remove FragmentKey. No longer using.

--- app/src/main/java/com/codingwithmitch/espressodaggerexamples/di/FragmentModule.kt
+++ app/src/main/java/com/codingwithmitch/espressodaggerexamples/di/FragmentModule.kt
@@ -1,18 +1,11 @@
 package com.codingwithmitch.espressodaggerexamples.di
 
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
-import com.codingwithmitch.daggermultifeature.feature1.di.keys.MainFragmentKey
 import com.codingwithmitch.espressodaggerexamples.fragments.MainFragmentFactory
-import com.codingwithmitch.espressodaggerexamples.ui.DetailFragment
-import com.codingwithmitch.espressodaggerexamples.ui.FinalFragment
-import com.codingwithmitch.espressodaggerexamples.ui.ListFragment
 import com.codingwithmitch.espressodaggerexamples.util.GlideManager
 import com.codingwithmitch.espressodaggerexamples.viewmodels.MainViewModelFactory
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
-import dagger.multibindings.IntoMap
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
 import javax.inject.Singleton
