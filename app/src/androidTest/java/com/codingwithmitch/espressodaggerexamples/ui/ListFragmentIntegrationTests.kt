29269531759c48ca18e54778772485bef8f275d1
mitch <mitch@tabian.ca>
2020-03-02 15:25:01 -0800

    Lecture: Restoring the InstanceState

--- app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentIntegrationTests.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentIntegrationTests.kt
@@ -248,6 +248,44 @@ class ListFragmentIntegrationTests: BaseMainActivityTests() {
             .check(doesNotExist())
     }
 
+    @Test
+    fun isInstanceStateSavedAndRestored_OnDestroyActivity() {
+
+        val app = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext
+            .applicationContext as TestBaseApplication
+
+        val apiService = configureFakeApiService(
+            blogsDataSource = Constants.BLOG_POSTS_DATA_FILENAME,
+            categoriesDataSource = Constants.CATEGORIES_DATA_FILENAME,
+            networkDelay = 0L,
+            application = app
+        )
+
+        configureFakeRepository(apiService, app)
+
+        injectTest(app)
+
+        val scenario = launchActivity<MainActivity>()
+
+        onView(withId(R.id.recycler_view))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions.scrollToPosition<BlogPostListAdapter.BlogPostViewHolder>(8)
+        )
+
+        onView(withText("Blake Posing for his Website"))
+            .check(matches(isDisplayed()))
+
+        scenario.recreate()
+
+        onView(withText("Blake Posing for his Website")).check(matches(isDisplayed()))
+
+    }
+
+
     override fun injectTest(application: TestBaseApplication) {
         (application.appComponent as TestAppComponent)
             .inject(this)
