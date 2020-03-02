b3aaec3a00f62b5ca7616b9d6b050fd0faf607df
mitch <mitch@tabian.ca>
2020-03-02 15:19:11 -0800

    Lecture: RecyclerViewActions, Scrolling, Clicking, Updating

--- app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentIntegrationTests.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentIntegrationTests.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
 import com.codingwithmitch.espressodaggerexamples.R
 import com.codingwithmitch.espressodaggerexamples.TestBaseApplication
 import com.codingwithmitch.espressodaggerexamples.di.TestAppComponent
+import com.codingwithmitch.espressodaggerexamples.ui.viewmodel.state.MainViewState
 import com.codingwithmitch.espressodaggerexamples.util.Constants
 import com.codingwithmitch.espressodaggerexamples.util.EspressoIdlingResourceRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -108,6 +111,141 @@ class ListFragmentIntegrationTests: BaseMainActivityTests() {
 
         onView(withSubstring("All"))
             .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun checkListData_testScrolling() {
+
+        val app = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext
+            .applicationContext as TestBaseApplication
+
+        val apiService = configureFakeApiService(
+            blogsDataSource = Constants.BLOG_POSTS_DATA_FILENAME,
+            categoriesDataSource = Constants.CATEGORIES_DATA_FILENAME,
+            networkDelay = 0L,
+            application = app
+        )
+
+        configureFakeRepository(apiService, app)
+
+        injectTest(app)
+
+        val scenario = launchActivity<MainActivity>()
+
+        val recyclerView = onView(withId(R.id.recycler_view))
+
+        recyclerView.check(matches(isDisplayed()))
+
+        recyclerView.perform(
+            RecyclerViewActions.scrollToPosition<BlogPostListAdapter.BlogPostViewHolder>(5)
+        )
+        onView(withText("Mountains in Washington")).check(matches(isDisplayed()))
+
+        recyclerView.perform(
+            RecyclerViewActions.scrollToPosition<BlogPostListAdapter.BlogPostViewHolder>(8)
+        )
+        onView(withText("Blake Posing for his Website")).check(matches(isDisplayed()))
+
+        recyclerView.perform(
+            RecyclerViewActions.scrollToPosition<BlogPostListAdapter.BlogPostViewHolder>(0)
+        )
+        onView(withText("Vancouver PNE 2019")).check(matches(isDisplayed()))
+
+        onView(withId(R.id.no_data_textview))
+            .check(matches(withEffectiveVisibility(Visibility.GONE)))
+    }
+
+    @Test
+    fun checkListData_onCategoryChange_toEarthporn(){
+        val app = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext
+            .applicationContext as TestBaseApplication
+
+        val apiService = configureFakeApiService(
+            blogsDataSource = Constants.BLOG_POSTS_DATA_FILENAME,
+            categoriesDataSource = Constants.CATEGORIES_DATA_FILENAME,
+            networkDelay = 0L,
+            application = app
+        )
+
+        configureFakeRepository(apiService, app)
+
+        injectTest(app)
+
+        val scenario = launchActivity<MainActivity>().onActivity { mainActivity ->
+            val toolbar: Toolbar = mainActivity.findViewById(R.id.tool_bar)
+
+            mainActivity.viewModel.viewState.observe(mainActivity, object: Observer<MainViewState>{
+                override fun onChanged(viewState: MainViewState?) {
+                    if(viewState?.activeJobCounter?.size == 0){
+                        toolbar.showOverflowMenu()
+                        mainActivity.viewModel.viewState.removeObserver(this)
+                    }
+                }
+            })
+        }
+
+        // click "earthporn" category from menu
+        val CATEGORY_NAME = "earthporn"
+        onView(withText(CATEGORY_NAME)).perform(click())
+
+        onView(withText("Mountains in Washington"))
+            .check(matches(isDisplayed()))
+
+        onView(withText("France Mountain Range"))
+            .check(matches(isDisplayed()))
+
+        onView(withText("Vancouver PNE 2019"))
+            .check(doesNotExist())
+    }
+
+
+    @Test
+    fun checkListData_onCategoryChange_toFun(){
+        val app = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext
+            .applicationContext as TestBaseApplication
+
+        val apiService = configureFakeApiService(
+            blogsDataSource = Constants.BLOG_POSTS_DATA_FILENAME,
+            categoriesDataSource = Constants.CATEGORIES_DATA_FILENAME,
+            networkDelay = 0L,
+            application = app
+        )
+
+        configureFakeRepository(apiService, app)
+
+        injectTest(app)
+
+        val scenario = launchActivity<MainActivity>().onActivity { mainActivity ->
+            val toolbar: Toolbar = mainActivity.findViewById(R.id.tool_bar)
+
+            mainActivity.viewModel.viewState.observe(mainActivity, object: Observer<MainViewState>{
+                override fun onChanged(viewState: MainViewState?) {
+                    if(viewState?.activeJobCounter?.size == 0){
+                        toolbar.showOverflowMenu()
+                        mainActivity.viewModel.viewState.removeObserver(this)
+                    }
+                }
+            })
+        }
+
+        // click "fun" category from menu
+        val CATEGORY_NAME = "fun"
+        onView(withText(CATEGORY_NAME)).perform(click())
+
+        onView(withText("My Brother Blake"))
+            .check(matches(isDisplayed()))
+
+        onView(withText("Vancouver PNE 2019"))
+            .check(matches(isDisplayed()))
+
+        onView(withText("France Mountain Range"))
+            .check(doesNotExist())
     }
 
     override fun injectTest(application: TestBaseApplication) {
