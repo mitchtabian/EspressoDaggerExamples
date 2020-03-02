cc4e1175ec8281731d8972162b30cf4ffe4b87d2
mitch <mitch@tabian.ca>
2020-03-02 15:11:13 -0800

    Lecture: Accessing Activity Instance from Within a Test

--- app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentIntegrationTests.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/ui/ListFragmentIntegrationTests.kt
@@ -1,7 +1,10 @@
 package com.codingwithmitch.espressodaggerexamples.ui
 
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -59,6 +62,52 @@ class ListFragmentIntegrationTests: BaseMainActivityTests() {
 
         onView(withId(R.id.no_data_textview))
             .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+
+
+    // if query for categories returns an empty list, the user should still see
+    // the menu item "All"
+    @Test
+    fun isCategoryListEmpty() {
+
+        val app = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext
+            .applicationContext as TestBaseApplication
+
+        val apiService = configureFakeApiService(
+            blogsDataSource = Constants.BLOG_POSTS_DATA_FILENAME,
+            categoriesDataSource = Constants.EMPTY_LIST, // empty list of data
+            networkDelay = 0L,
+            application = app
+        )
+
+        configureFakeRepository(apiService, app)
+
+        injectTest(app)
+
+        val scenario = launchActivity<MainActivity>().onActivity { mainActivity ->
+            val toolbar: Toolbar = mainActivity.findViewById(R.id.tool_bar)
+
+            // wait for jobs to complete to open the menu
+            mainActivity.viewModel.viewState.observe(mainActivity, Observer { viewState ->
+                if(viewState.activeJobCounter.size == 0){
+                    toolbar.showOverflowMenu()
+                }
+            })
+        }
+
+        onView(withSubstring("earthporn"))
+            .check(doesNotExist())
+
+        onView(withSubstring("dogs"))
+            .check(doesNotExist())
+
+        onView(withSubstring("fun"))
+            .check(doesNotExist())
+
+        onView(withSubstring("All"))
+            .check(matches(isDisplayed()))
     }
 
     override fun injectTest(application: TestBaseApplication) {
