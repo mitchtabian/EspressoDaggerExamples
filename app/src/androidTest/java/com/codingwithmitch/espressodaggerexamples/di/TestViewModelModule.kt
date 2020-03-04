a4794db92b859bfa559451ff1b8db665b6ee961c
mitch <mitch@tabian.ca>
2020-03-04 14:15:48 -0800

    refactor ViewModelProvider

--- app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/di/TestViewModelModule.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressodaggerexamples/di/TestViewModelModule.kt
@@ -1,31 +1,30 @@
 package com.codingwithmitch.espressodaggerexamples.di
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.codingwithmitch.daggermultifeature.feature1.di.keys.MainViewModelKey
+import com.codingwithmitch.espressodaggerexamples.repository.FakeMainRepositoryImpl
 import com.codingwithmitch.espressodaggerexamples.viewmodels.FakeMainViewModelFactory
-import com.codingwithmitch.espressodaggerexamples.ui.viewmodel.MainViewModel
-import dagger.Binds
 import dagger.Module
-import dagger.multibindings.IntoMap
+import dagger.Provides
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
+import javax.inject.Singleton
 
 @ExperimentalCoroutinesApi
 @InternalCoroutinesApi
 @Module
-abstract class TestViewModelModule {
+object TestViewModelModule {
 
-    @Binds
-    abstract fun bindViewModelFactory(vmFactory: FakeMainViewModelFactory): ViewModelProvider.Factory
-
-    @Binds
-    @IntoMap
-    @MainViewModelKey(MainViewModel::class)
-    abstract fun bindMainViewModel(viewModel: MainViewModel): ViewModel
-
+    @JvmStatic
+    @Singleton
+    @Provides
+    fun provideViewModelFactory(
+        mainRepository: FakeMainRepositoryImpl
+    ): ViewModelProvider.Factory{
+        return FakeMainViewModelFactory(
+            mainRepository
+        )
+    }
 }
-
 
 
 
